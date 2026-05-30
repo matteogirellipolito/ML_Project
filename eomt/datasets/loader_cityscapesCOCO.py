de9bf2edf860c7f0925494b6d180d2c8fe33503a
matteogirellipolito <s355395@studenti.polito.it>
2026-05-30 12:34:09 +0200

    Creation of loader of dataset Cityscapes-COCO

---
+++ eomt/datasets/loader_cityscapesCOCO.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+from PIL import Image
+
+from torch.utils.data import Dataset
+
+import numpy as np
+
+class CityscapesOE(Dataset):
+    def __init__(
+        self,
+        image_dir,
+        anomaly_dir,
+    ):
+        self.images = sorted(list(Path(image_dir).glob("*.png")))
+        self.masks = sorted(list(Path(anomaly_dir).glob("*.png")))
+
+    def _len(self):
+        return len(self.images)
+
+    def _getitem(self,idx):
+        img = np.array(Image.open(self.images[idx]).convert("RGB"))
+        anomaly = np.array(Image.open(self.masks[idx]))
+        anomaly = anomaly > 0
+
+        return img, anomaly
