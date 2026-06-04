e64d3d3ed0bb78e7fbd3eddcf059f94d84aabab7
matteogirellipolito <s355395@studenti.polito.it>
2026-06-04 15:33:44 +0200

    Anomaly mask mishaped 2nd

--- eomt/datasets/loader_cityscapesCOCO.py
+++ eomt/datasets/loader_cityscapesCOCO.py
@@ -6,7 +6,7 @@ import torch
 
 from torch.utils.data import Dataset
 from torchvision import tv_tensors
-import torchvision.transforms.functional as TF
+
 
 class CityscapesCOCO(Dataset):
 
@@ -17,71 +17,65 @@ class CityscapesCOCO(Dataset):
         semantic_dataset=None,
     ):
 
-        image_dir = Path(image_dir)
-        anomaly_dir = Path(anomaly_dir)
-
-        self.images = sorted([p for p in image_dir.rglob("*.png") if p.is_file()])
-        self.masks = sorted([p for p in anomaly_dir.rglob("*.png") if p.is_file()])
-
-        print("FOUND IMAGES:", len(self.images))
-        print("FOUND MASKS:", len(self.masks))
-
-        assert len(self.images) > 0
-        assert len(self.images) == len(self.masks)
-
+        self.images = sorted(Path(image_dir).rglob("*.png"))
+        self.masks = sorted(Path(anomaly_dir).rglob("*.png"))
         self.semantic_dataset = semantic_dataset
+
+        print("FOUND IMAGES:",len(self.images))
+        print("FOUND MASKS:",len(self.masks))
 
     def __len__(self):
         return len(self.images)
 
+    def __getitem__(self,idx):
+        sem_img, target = self.semantic_dataset[idx]
+
+        contam = np.array(Image.open(self.images[idx]).convert("RGB"))
+        contam = torch.from_numpy(contam).permute(2,0,1).float()/255.
+        contam = torch.nn.functional.interpolate(
+            contam.unsqueeze(0),
+            size=sem_img.shape[-2:],
+            mode="bilinear",
+            align_corners=False
+        ).squeeze(0)
+
+        anomaly = np.array(Image.open(self.masks[idx])) > 0
+        anomaly = torch.tensor(anomaly,dtype=torch.float32)[None,None]
+        anomaly = torch.nn.functional.interpolate(
+            anomaly,
+            size=sem_img.shape[-2:],
+            mode="nearest"
+        )[0,0].bool()
+
+        target["anomaly_mask"] = tv_tensors.Mask(anomaly)
+
+        return contam, target
+    
+"""
+File di prima:
+
+
+FILE USATO:
+/content/ML_Project/eomt/datasets/loader_cityscapesCOCO.py
+
+INIZIO __getitem__:
+
     def __getitem__(self, idx):
 
-        img = Image.open(
-            self.images[idx]
-        ).convert("RGB")
-
-        anomaly = Image.open(
-            self.masks[idx]
-        )
+        img = Image.open(self.images[idx]).convert("RGB")
+        anomaly = np.array(Image.open(self.masks[idx])) > 0
+        anomaly = tv_tensors.Mask(torch.tensor(anomaly,dtype=torch.bool))
 
         if self.semantic_dataset is None:
-
-            img = torch.from_numpy(
-                np.array(img)
-            ).permute(2,0,1).float()/255.
-
-            anomaly = torch.tensor(
-                np.array(anomaly) > 0,
-                dtype=torch.bool
-            )
-
-            anomaly = tv_tensors.Mask(anomaly)
+            img = torch.from_numpy(np.array(img)).permute(2,0,1).float() / 255.0
 
             return img, anomaly
 
         _, target = self.semantic_dataset[idx]
-
-        img = torch.from_numpy(
-            np.array(img)
-        ).permute(2,0,1).float()/255.
-
-        target_h, target_w = target["masks"].shape[-2:]
-
-        anomaly = TF.resize(
-            anomaly,
-            [target_h, target_w],
-            interpolation=TF.InterpolationMode.NEAREST
-        )
-
-        anomaly = np.array(anomaly) > 0
-
-        anomaly = tv_tensors.Mask(
-            torch.tensor(
-                anomaly,
-                dtype=torch.bool
-            )
-        )
-
+        img = torch.from_numpy(np.array(img)).permute(2,0,1).float() / 255.0
         target["anomaly_mask"] = anomaly
 
         return img, target
+
+
+"""
