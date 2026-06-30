a77d3c978ef734d713117f5277a7c9bdba07a75c
matteogirellipolito <s355395@studenti.polito.it>
2026-06-30 18:51:35 +0200

    Deleted old files

--- eomt/datasets/cityscapes_semantic_oe.py
+++ eomt/datasets/cityscapes_semantic_oe.py
@@ -1,14 +1,22 @@
-from torch.utils.data import DataLoader
+# =============================================================================
+# VERSIONE SPERIMENTALE (Opzione B') - NON e' l'originale.
+# Differenze rispetto a cityscapes_semantic_oe.py:
+#   - usa PROVA_loader_prova (loader con transform unico sulla terna)
+#   - passa al loader anche gt_dir
+#   - FORWARD CORRETTO di prob_oe al loader (nell'originale era ignorato)
+# =============================================================================
 
 from datasets.cityscapes_semantic import CityscapesSemantic
-#from datasets.loader_cityscapesCOCO import CityscapesCOCO
-from datasets.loader_prova import CityscapesCOCO
+from datasets.PROVA_loader_prova import CityscapesCOCO
+
 
 class CityscapesSemanticOE(CityscapesSemantic):
     def __init__(
         self,
         contaminated_images,
         anomaly_masks,
+        gt_dir,
+        prob_oe=0.1,
         **kwargs
     ):
 
@@ -16,6 +24,8 @@ class CityscapesSemanticOE(CityscapesSemantic):
 
         self.contaminated_images = contaminated_images
         self.anomaly_masks = anomaly_masks
+        self.gt_dir = gt_dir
+        self.prob_oe = prob_oe
 
     def setup(
         self,
@@ -27,7 +37,9 @@ class CityscapesSemanticOE(CityscapesSemantic):
         self.cityscapes_train_dataset = CityscapesCOCO(
             image_dir=self.contaminated_images,
             anomaly_dir=self.anomaly_masks,
+            gt_dir=self.gt_dir,
             semantic_dataset=self.cityscapes_train_dataset,
+            prob_oe=self.prob_oe,
         )
 
         return self
